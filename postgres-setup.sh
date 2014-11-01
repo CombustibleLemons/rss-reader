b880f12156023d2921658a88e33640d20df73deb
Justyn Harriman <justyn.harriman@vgbio.com>
2014-11-01 11:52:15 -0500

    Spelling error with postgres-setup.sh

--- postgres-setup.sh
+++ postgres-setup.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 sudo -u postgres -H sh -c "createdb feeddb"
 sudo -u postgres -H sh -c "createuser -P combustible"
-sudo -u postgres -H sh -c "psql -c 'GRANT ALL PRIVLEGES ON DATABASE feeddb TO combustible;'"
+sudo -u postgres -H sh -c "psql -c 'GRANT ALL PRIVILEGES ON DATABASE feeddb TO combustible;'"
