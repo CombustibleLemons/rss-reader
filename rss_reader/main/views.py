9b8644c19ac6191d8482ba79ac8b4f48e6d5e9ce
jzakaria <jzakaria007@yahoo.com>
2014-11-01 12:28:05 -0500

    added templates and static files

--- rss_reader/main/views.py
+++ rss_reader/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.template import RequestContext, loader
+from django.contrib.auth.decorators import login_required
+import logging
 
-# Create your views here.
+
+def index(request):
+	template = "index.html"
+	return render(request, template, {})
