43bf77233f4535e12013f4e51a253e07e19e05fa
Justyn Harriman <justyn.harriman@vgbio.com>
2014-11-02 11:27:04 -0600

    Setup testing framework

--- rss_reader/main/tests.py
+++ rss_reader/main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from main.models import Feed, Post
 
 # Create your tests here.
+class FeedTestCase(TestCase):
+    def setUp(self):
+        self.testUrl = "http://home.uchicago.edu/~jharriman/example-rss.xml"
+
+    def testCreateByUrl(self):
+        """Test that createByUrl constructor accurately creates a Feed object"""
+        feed = Feed.createByUrl(self.testUrl)
+        self.assertEqual(feed.URL, "http://rss.nytimes.com/services/xml/rss/nyt/US.xml")
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        pass
