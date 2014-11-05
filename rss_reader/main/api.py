6ae4ea2885354b5e4984b5547f40abfbc1b06c00
Justyn Harriman <justyn.harriman@vgbio.com>
2014-11-05 12:37:26 -0600

    Adding Lucia's models to angular framework

--- rss_reader/main/api.py
+++ rss_reader/main/api.py
@@ -3,6 +3,27 @@ from rest_framework import generics, permissions
 from .serializers import FeedSerializer, PostSerializer
 from .models import Feed, Post
 
+class TopicList(generics.ListCreateAPIView):
+    model = Topic
+    serializer_class = TopicSerializer
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    # We can limit the fields that we display here so that it is comprehensible to the user.
+
+class TopicDetail(generics.RetrieveAPIView):
+    model = Topic
+    serializer_class = TopicSerializer
+
+class TopicFeedList(generics.ListAPIView):
+    model = Feed
+    serializer_class = FeedSerializer
+    def get_queryset(self):
+        Topic_id = self.kwargs.get("pk")
+        queryset = super(TopicFeedList, self).get_queryset()
+        return queryset.filter(Topic__pk=Topic_id)
+
 
 class FeedList(generics.ListCreateAPIView):
     model = Feed
@@ -11,7 +32,7 @@ class FeedList(generics.ListCreateAPIView):
         permissions.AllowAny
     ]
 
-    # We can limit the fields that we display here so that it is comprehensible to the user. 
+    # We can limit the fields that we display here so that it is comprehensible to the user.
 
 class FeedDetail(generics.RetrieveAPIView):
     model = Feed
