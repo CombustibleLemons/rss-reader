d6608689f9c79092c41e84f0d6d3fb5de1159f11
James DelVesco <jamdel@uchicago.edu>
2014-11-05 18:00:04 -0600

    added test notes

--- rss_reader/main/static/js/controllers.js
+++ rss_reader/main/static/js/controllers.js
@@ -2,15 +2,24 @@
 
 /* Controllers */
 
-angular.module('main.controllers', []).
-    controller('FeedCtrl', function($scope, $http) {
+angular.module('main.controllers', [])
+    .controller('FeedCtrl', function($scope, $http) { //scope is an angular template, from base.html, index.html
       $http.get('feeds/4/posts').success(function(data) {
-        $scope.posts = data;
+        $scope.posts = data; //pushes data into the scope, so now we can do things with scope.posts
       });
+
+      /* fetchPosts()
+         returns a list of feeds, all of which are feed controllers */
     })
     .controller('PostCtrl', function($scope, $http) {
       $http.get('posts/26').success(function(data){
-        $scope.detailed_post = data;
+        $scope.detailed_post = data; //pushes things to detailed post
       });
+
+      /* Other post controller functions go here */
+      //expand()
+      //minimize()
+      //isExpanded()
+
     });
 //*/
