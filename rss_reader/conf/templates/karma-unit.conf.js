5e94c151635f579702812d49c8ed4e60a3d50521
Devon Compton <devon.jc.compton@gmail.com>
2014-11-05 20:34:03 -0600

    karmaunit conf and controllerspecjs

--- rss_reader/conf/templates/karma-unit.conf.js
+++ rss_reader/conf/templates/karma-unit.conf.js
@@ -1,75 +1,25 @@
-// Karma configuration
-// Generated on Wed Nov 05 2014 09:24:05 GMT-0600 (CST)
+basePath = '../';
 
-module.exports = function(config) {
-  config.set({
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  '{{ djangular_root }}/static/lib/angular/angular.js',
+  '{{ djangular_root }}/static/lib/angular/angular-*.js',
+  '{{ djangular_root }}/tests/lib/angular/angular-mocks.js',
+  '{{ djangular_root }}/templates/djangular_module.js',
+// NOTE: Place other Javascript dependencies here.
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../',
+// JS Files to be tested: {% for app_path in app_paths %}
+  '{{ app_path }}/app/js/**/*.js', // {% endfor %}
+// JS Tests: {% for app_path in app_paths %}
+  '{{ app_path }}/tests/unit/**/*.js', // {% endfor %}
+];
 
+autoWatch = true;
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+browsers = ['Chrome'];
 
-
-    // list of files / patterns to load in the browser
-    files: [
-	    '{{ djangular_root }}/static/lib/angular/angular.js',
-	    '{{ djangular_root }}/static/lib/angular/angular-*.js',
-	    '{{ djangular_root }}/tests/lib/angular/angular-mocks.js',
-	    '{{ djangular_root }}/templates/djangular_module.js',
-      'http://code.jquery.com/jquery-2.1.1.js',
-	    // NOTE: Place other Javascript dependencies here.
-
-	    // JS Files to be tested: {% for app_path in app_paths %}
-	    '{{ app_path }}/static/js/**/*.js', // {% endfor %}
-	    // JS Tests: {% for app_path in app_paths %}
-	    '{{ app_path }}/tests/unit/**/*.js', // {% endfor %}
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
+junitReporter = {
+  outputFile: 'test_out/unit.xml',
+  suite: 'unit'
 };
