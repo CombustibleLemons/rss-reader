ad0ecbf225387c479c7e2bb1503eeb4895a86e66
Devon Compton <devon.jc.compton@gmail.com>
2014-11-05 22:17:51 -0600

    Committing some controller tests

--- rss_reader/main/static/js/controllers.js
+++ rss_reader/main/static/js/controllers.js
@@ -3,23 +3,24 @@
 /* Controllers */
 
 angular.module('main.controllers', [])
-    .controller('FeedCtrl', function($scope, $http) { //scope is an angular template, from base.html, index.html
-      $http.get('feeds/4/posts').success(function(data) {
-<<<<<<< HEAD
-        $scope.posts = data; //pushes data into the scope, so now we can do things with scope.posts
-      });
-
-      /* fetchPosts()
-         returns a list of feeds, all of which are feed controllers */
-=======
+  .controller('UserController', function($scope, $http) {
+    $http.get('user/').success(function(data) {
+      $scope.user = data;
+    });
+    $scope.addTopic = function(topicName) {
+      // THIS IS WHERE A FUNCTION GOES, DOO DAH, DOO DAH
+    };
+  })
+    .controller('FeedController', function(id, $scope, $http) { //scope is an angular template, from base.html, index.html
+      
+      $http.get('feeds/' + id + "/posts").success(function(data) {
         $scope.posts = data;
       };
       $scope.addFeedByUrl = function(url){
 
       };)
->>>>>>> angular-framework
     })
-    .controller('PostCtrl', function($scope, $http) {
+    .controller('PostController', function($scope, $http) {
       $http.get('posts/26').success(function(data){
         $scope.detailed_post = data; //pushes things to detailed post
       });
