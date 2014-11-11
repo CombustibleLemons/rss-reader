f8057ee0ef52ac643899034d330824dccf961b19
Justyn Harriman <jharriman@uchicago.edu>
2014-11-11 17:05:02 -0600

    Cleanup refresh from server

--- rss_reader/main/static/js/controllers.js
+++ rss_reader/main/static/js/controllers.js
@@ -3,11 +3,17 @@
 /* Controllers */
 
 angular.module('main.controllers', [])
-  .controller('UserController', function($scope, $rootScope, $http, $timeout, APIService) {
+  .controller('UserController', function($scope, $rootScope, $http, $timeout, $q, APIService) {
+    $scope.refreshInterval = 5;
     $scope.refreshUser = function(){
-      return APIService.getUser().then(function(user){
+      // Return a function that will keep refreshing the feeds
+      var promise = APIService.getUser().then(function(user){
         $scope.user = user;
       });
+      $timeout(function(){$scope.refreshUser();}, $scope.refreshInterval * 1000);
+      // I have no idea where this returns to when the function calls itself, and what
+      // Angular does to garbage collect. But it works.
+      return promise;
     };
     $scope.getTopicIds = function(){
       if (this.user == null){
@@ -18,11 +24,14 @@ angular.module('main.controllers', [])
         return promise;
       }
       else{
-        return function(){return $scope.user["topics"]};
+        // Return a promise that we can resolve immediately
+        var deferred = $q.defer();
+        deferred.resolve($scope.user["topics"]);
+        return deferred.promise;
       }
     };
   })
-  .controller('NavigationController', function($scope, $http, APIService) {
+  .controller('NavigationController', function($scope, $http, $timeout, APIService) {
     // Attributes
     $scope.expandedIndex = 0;
 
@@ -33,21 +42,19 @@ angular.module('main.controllers', [])
     $scope.removeTopic = function(topicName) {
       // THIS IS WHERE THE NEXT ONE GOES, DOO DAH, DOO DAH
     };
-    $scope.fetchTopics = function($scope, $http) {
+    $scope.fetchTopics = function() {
       // Chicken and Egg problem, the UserController may not load before this class so we need to force a promise
       // Ask the UserController if it has data yet
       $scope.$parent.getTopicIds().then(function(topic_ids){
-        APIService.getTopicsByIds(topic_ids).then(function(topics){
-          $scope.topics = topics;
-        });
-        /* var topic_list = [];
-        for (var i = 0; i < data.length; i++){
-          $http.get('topics/' + data[i]).success(function(data){
-            topic_list.push(data);
+        // TODO: Compare ids to see if we need to update the topics set
+        if ($scope.topicIds != topic_ids){
+          $scope.topicIds = topic_ids;
+          APIService.getTopicsByIds(topic_ids).then(function(topics){
+            $scope.topics = topics;
           });
-        }*/
-        //$scope.topics = topic_list;
+        }
       });
+      $timeout(function(){$scope.fetchTopics();}, $scope.refreshInterval * 1000);
     };
     $scope.expandTopic = function(index) {
       $scope.expandedIndex = index
@@ -55,7 +62,7 @@ angular.module('main.controllers', [])
     $scope.minimizeTopic = function(index) {
       // TODO: Decide if there is ANYTHING to do here or if Angular covers it all for us.
     };
-    $scope.fetchTopics($scope, $http);
+    $scope.fetchTopics();
   })
   .controller('SearchController', function($scope, $http) {
     $scope.addFeed = function() { // formerly passed url as an argument
@@ -68,14 +75,16 @@ angular.module('main.controllers', [])
     };
   })
   .controller('TopicController', function($scope, $http, $timeout, APIService) {
-    $scope.topic = $scope.$parent.topics[$scope.$parent.$index];
-    $scope.refreshInterval = 10;
     $scope.addFeedToTopic = function(url) {
       // function function function
     };
     $scope.editName = function(newName) {
       // FUNCTION YEAH
     };
+    $scope.refreshTopic = function(){
+      $scope.topic = $scope.$parent.topics[$scope.$parent.$index];
+      $timeout(function(){$scope.refreshTopic();}, $scope.refreshInterval * 1000);
+    }
     $scope.fetchFeeds = function($scope, $http) {
       // Get the feed IDs
       var feed_ids = $scope.topic["feeds"];
@@ -91,6 +100,7 @@ angular.module('main.controllers', [])
       // expand the feed (one feed per topic in iteration one)
     };
     $scope.fetchFeeds($scope, $http);
+    $scope.refreshTopic();
   })
   .controller('FeedController', function($scope, $http) { //scope is an angular template, from base.html, index.html
     $http.get('feeds/' + id + "/posts").success(function(data) {
