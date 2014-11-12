5a3746d7effbfde914ac322f096159be9f5edd63
Devon Compton <devon.jc.compton@gmail.com>
2014-11-11 20:06:12 -0600

    Committing before merging

--- rss_reader/main/tests/unit/controllersSpec.js
+++ rss_reader/main/tests/unit/controllersSpec.js
@@ -1,12 +1,17 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-
+/*
 describe("Navigation controllers", function() {
     beforeEach(module("main.controllers"));
-    var scope, navigationController;
-    scope = $rootScope.$new();
-    navigationController = $controller('NavigationController', {$scope: scope});
+    var scope;
+
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        $controller('NavigationController', {$scope: scope});
+
+        scope.$digest();
+    }));
 
     it("should add topics", function() {
         var origTopicSet = scope.user.topic_set;
@@ -40,30 +45,40 @@ describe("Navigation controllers", function() {
         expect(scope.minimize('foo')).toBe(true);
         expect(scope.view.div.ul[feeds].css('display')).toBe('none'); // probably even more wrong syntax
     });
-});
-
-describe("Search controllers", function() {
+});*/
+    
+describe("Search controllers", function($rootScope) {
     beforeEach(module("main.controllers"));
-    var scope, searchController;
-    scope = $rootScope.$new();
-    searchController = $controller('SearchController', {$scope: scope});
+    var scope, httpBackend;
+
+    beforeEach(inject(function($controller, $rootScope, $httpBackend) {
+        scope = $rootScope.$new();
+        $controller('SearchController', {$scope: scope});
+
+        scope.$digest();
+    }));
 
     it("should add feeds to uncategorized", function() {
-        var originalFeedSet = scope.user.topic_set['uncategorized']; // again, bad syntax
-        expect(scope.addFeed('pretendthisisavalidurl')).toBe(true);
-        var newFeedSet = scope.user.topic_set['uncategorized']; // yet again
-        expect(originalFeedSet.length).toEqual(newFeedSet.length + 1);
+        scope.query = 'http://www.polygon.com/rss/index.xml';
+        expect(scope.addFeed()).toBe(true);
+        // var newFeedSet = scope.user.topic_set['uncategorized']; // yet again
+        // expect(originalFeedSet.length).toEqual(newFeedSet.length + 1);
         // expect feed to be in uncategorized topic, uncertain of syntax at this time
     });
 });
-
-describe("Topic controllers", function() {
+/*
+describe("Topic controllers", function($rootScope) {
     beforeEach(module("main.controllers"));
-    var scope, topicController;
-    scope = $rootScope.$new();
-    topicController = $controller('TopicController', {$scope: scope});
+    var scope;
 
-    it("should edit names", function(), {
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        $controller('TopicController', {$scope: scope});
+
+        scope.$digest();
+    }));
+
+    it("should edit names", function() {
         var originalName = scope.topic.name;
         var changedName = scope.topic.editName("bar");
         expect(changedName).toBe(true);
@@ -101,11 +116,16 @@ describe("Topic controllers", function() {
     });
 });
 
-describe("Feed controllers", function() {
+describe("Feed controllers", function($rootScope) {
     beforeEach(module("main.controllers"));
-    var scope, feedController;
-    scope = $rootScope.$new();
-    feedController = $controller('FeedController', {$scope: scope});
+    var scope;
+
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        $controller('FeedController', {$scope: scope});
+
+        scope.$digest();
+    }));
 
     it("should fetch posts", function() {
         var posts = scope.fetchPosts(validFeed);
@@ -113,12 +133,18 @@ describe("Feed controllers", function() {
         expect(scope.foofeed.post_set.length).toBeGreaterThan(0);
         expect(scope.view.div.ul['posts']).toBeGreaterThan(0); // syntax???
     });
+});
 
-describe("Post controllers", function() {
+describe("Post controllers", function($rootScope) {
     beforeEach(module("main.controllers"));
-    var scope, postController;
-    scope = $rootScope.$new();
-    postController = $controller('PostController', {$scope: scope});
+    var scope;
+
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        $controller('PostController', {$scope: scope});
+
+        scope.$digest();
+    }));
 
     it("should expand posts", function() {
         var height = scope.view.div.attr('height'); // again, syntax
@@ -143,4 +169,4 @@ describe("Post controllers", function() {
         expect(scope.view.div.height).toBeLessThan(height); // syntax
         expect(scope.expanded).toBe(false);
     });
-});
+});*/
