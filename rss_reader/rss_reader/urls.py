1f31bc9f70ec0a983fd93d262a7645785379ccca
jzakaria <jzakaria007@yahoo.com>
2014-11-15 18:10:25 -0600

    Added dummy login page

--- rss_reader/rss_reader/urls.py
+++ rss_reader/rss_reader/urls.py
@@ -44,6 +44,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
 	# Main
-    url(r'^', MainViews.index),
+    url(r'^$', MainViews.index),
+
+    url(r'^login/', MainViews.login),
 
    )
